d1045435169aa961985bc07263889f54d0df65e4
Z M Asif Jamil <80680790+FurySimo@users.noreply.github.com>
2021-03-17 11:43:14 +0600

    Add files via upload

---
+++ User Input/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace User_Input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter username:");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Username is:" + userName);
+            Console.WriteLine("Enter your age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your age is:" + age);
+            Console.WriteLine("Enter your Bloodgroup:");
+            string BloodGroup = Console.ReadLine();
+            Console.WriteLine  ("Bloodgroup is:" + BloodGroup);
+            Console.WriteLine("Solve this sum to prove you are human:");
+            Console.WriteLine("9+5");
+            int sum = Convert.ToInt32(Console.ReadLine());
+            if (sum == 14)
+                Console.WriteLine("Its correct");
+            else
+                Console.WriteLine("Its wrong. So you are a robot.");
+
+
+
+        }
+    }
+}
